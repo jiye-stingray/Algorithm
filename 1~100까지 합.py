33a67c34fbed84e9413cfc659215e4226c730284
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-09-08 19:01:23 +0900

    알고리즘

--- 1~100까지 합.py
+++ 1~100까지 합.py
@@ -10,3 +10,14 @@ s1 = 0
 for i in range(1,101):
     s1 += i
 print(s1)
+
+#재귀함수
+n1 = 100
+def Hap(n):
+    if n <= 1:
+        return 1
+    else:
+        return n + Hap(n-1)
+
+n1 = Hap(n1)
+print(n1)
